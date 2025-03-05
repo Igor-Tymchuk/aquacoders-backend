8d8220b79688563d898685c3ff2ab1978985a6a9
Igor Tymchuk <dinamit.shuher@gmail.com>
2025-03-05 10:38:20 +0200

    init commit

---
+++ src/routers/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const router = Router();
+
+console.log('hello router');
+
+// router.use('/contacts', contactsRouter);
+// router.use('/auth', authRouter);
+
+export default router;
