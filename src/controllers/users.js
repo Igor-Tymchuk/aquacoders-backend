f5a314bd3eb509fccd8a6a0b4ab38373b098618a
andrii-slipko <andreyslipko0703@gmail.com>
2025-03-06 23:54:40 +0200

    add current user, authenticate and fix schema

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -64,3 +64,13 @@ export const logoutUserController = async (req, res) => {
 
   res.status(204).send();
 };
+
+export const getCurrentUserController = async (req, res) => {
+  const { _id, name, email } = req.user;
+
+  res.json({
+    status: 200,
+    message: 'Current user retrieved successfully!',
+    data: { _id, name, email },
+  });
+};
--- src/validation/users.js
+++ src/validation/users.js
@@ -1,11 +1,7 @@
 import Joi from 'joi';
 
-export const signupUserSchema = Joi.object({
+export const inputUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
 
-export const signinUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
