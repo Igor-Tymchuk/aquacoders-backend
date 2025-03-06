0b9374fb8bbf254514340aeed7539794ca84dba5
Anastasiia Bulba <fedinanastya@gmail.com>
2025-03-06 17:32:43 +0200

    fix validation and controllers

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -8,103 +8,83 @@ import {
 
 // юзер додає запис про воду
 export const addWaterController = async (req, res, next) => {
-  try {
-    const { volume, date } = req.body;
+  const { volume, date } = req.body;
 
-    // Перетворення дати у формат Date перед відправленням у базу
-    const formattedDate = new Date(date);
+  // Перетворення дати у формат Date перед відправленням у базу
+  const formattedDate = new Date(date);
 
-    // const userId = req.user.id;
-    //   заглушка
-    const userId = '67a1f599b7ed372da1c632e0';
+  // const userId = req.user.id;
+  //   заглушка
+  const userId = '67a1f599b7ed372da1c632e0';
 
-    const newWater = await addWater(userId, volume, formattedDate);
+  const newWater = await addWater(userId, volume, formattedDate);
 
-    res.status(201).json({
-      status: 201,
-      message: 'Successfully added water record!',
-      data: newWater,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully added water record!',
+    data: newWater,
+  });
 };
 
 // юзєр редагує запис про воду
 export const updateWaterController = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { volume, date } = req.body;
-    // const userId = req.user.id;
-    //   заглушка
-    const userId = '67a1f599b7ed372da1c632e0';
+  const { id } = req.params;
+  const { volume, date } = req.body;
+  // const userId = req.user.id;
+  //   заглушка
+  const userId = '67a1f599b7ed372da1c632e0';
 
-    const updatedWater = await updateWater(userId, id, volume, date);
+  const updatedWater = await updateWater(userId, id, volume, date);
 
-    res.json({
-      status: 200,
-      message: 'Successfully updated water record!',
-      data: updatedWater,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully updated water record!',
+    data: updatedWater,
+  });
 };
 
 // юзер видаляє запис про воду
 export const deleteWaterController = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    // const userId = req.user.id;
-    //   заглушка
-    const userId = '67a1f599b7ed372da1c632e0';
+  const { id } = req.params;
+  // const userId = req.user.id;
+  //   заглушка
+  const userId = '67a1f599b7ed372da1c632e0';
 
-    await deleteWater(userId, id);
+  await deleteWater(userId, id);
 
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
+  res.status(204).send();
 };
 
 // отримує дані споживання за конкретний день
 export const getDailyWaterController = async (req, res, next) => {
-  try {
-    // const userId = req.user.id;
-    //   заглушка
-    // const userId = '67a1f599b7ed372da1c632e0';
+  // const userId = req.user.id;
+  //   заглушка
+  // const userId = '67a1f599b7ed372da1c632e0';
 
-    const { userId = '67a1f599b7ed372da1c632e0', date } = req.query;
+  const { userId = '67a1f599b7ed372da1c632e0', date } = req.query;
 
-    const waterEntries = await getDailyWater(userId, date);
+  const waterEntries = await getDailyWater(userId, date);
 
-    res.json({
-      status: 200,
-      message: 'Successfully retrieved daily water records!',
-      data: waterEntries,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully retrieved daily water records!',
+    data: waterEntries,
+  });
 };
 
 // отримує дані споживання за конкретний місяць
 export const getMonthlyWaterController = async (req, res, next) => {
-  try {
-    // const userId = req.user.id;
-    //   заглушка
-    const userId = '67a1f599b7ed372da1c632e0';
+  // const userId = req.user.id;
+  //   заглушка
+  const userId = '67a1f599b7ed372da1c632e0';
 
-    const { month } = req.query;
+  const { month } = req.query;
 
-    const waterEntries = await getMonthlyWater(userId, month);
+  const waterEntries = await getMonthlyWater(userId, month);
 
-    res.json({
-      status: 200,
-      message: 'Successfully retrieved monthly water records!',
-      data: waterEntries,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully retrieved monthly water records!',
+    data: waterEntries,
+  });
 };
